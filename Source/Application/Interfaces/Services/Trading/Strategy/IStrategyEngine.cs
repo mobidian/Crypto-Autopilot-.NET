292d765af391a7c881c9e3fa001f17c11fc5ff33
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-15 01:21:45 +0200

    Added a trading strategy engine interface

---
+++ Source/Application/Interfaces/Services/Trading/Strategy/IStrategyEngine.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces.Services.Trading;
+using Binance.Net.Objects.Models.Futures;
+using Domain.Models;
+using Skender.Stock.Indicators;
+
+namespace Application.Interfaces.Services.Trading.Strategy;
+
+public interface IStrategyEngine<T> : IDisposable where T : IQuote
+{
+    public ICfdTradingApiService ContractTrader { get; }
+
+    /// <summary>
+    /// // Reads and stores a given <see cref="T"/> array
+    /// </summary>
+    /// <param name="Candlesticks">The COMPLETED candlesticks</param>
+    /// <param name="LastOpenPrice">The open price of the unfinished candlestick</param>
+    public void SendData(T[] Candlesticks, decimal LastOpenPrice);
+
+    /// <summary>
+    /// Executes the strategy with respect to the previously given candlesticks array
+    /// </summary>
+    public void MakeMove();
+}
