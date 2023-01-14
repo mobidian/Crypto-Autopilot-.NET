95e7b10fe2d01a81c78b94e4d8ffb54ffc812d21
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-15 01:23:28 +0200

    Added a logger adapter interface for testability purposes

---
+++ Source/Application/Interfaces/Logging/ILoggerAdapter.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interfaces.Logging;
+
+public interface ILoggerAdapter<TType>
+{
+    public void LogInformation(string? message, params object?[] args);
+    
+    public void LogError(Exception? exception, string? message, params object?[] args);
+}
