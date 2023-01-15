8c9307a559ce38033c1ec285905b823c81121b9b
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-15 14:54:14 +0200

    Added mapping from domain objects to database entity objects

---
+++ Source/Application/Mapping/CandlestickMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Application.Data.Entities;
+
+using Domain.Models;
+
+namespace Application.Mapping;
+
+public static class CandlestickMappingExtensions
+{
+    public static CandlestickDbEntity ToDbEntity(this Candlestick candlestick) => new CandlestickDbEntity
+    {
+        CurrencyPair = candlestick.CurrencyPair.Name,
+        DateTime = candlestick.Date,
+        Open = candlestick.Open,
+        High = candlestick.High,
+        Low = candlestick.Low,
+        Close = candlestick.Close,
+    };
+}
---
+++ Source/Application/Mapping/FuturesOrderMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Application.Data.Entities;
+
+using Binance.Net.Objects.Models.Futures;
+
+namespace Application.Mapping;
+
+public static class FuturesOrderMappingExtensions
+{
+    public static FuturesOrderDbEntity ToDbEntity(this BinanceFuturesOrder futuresOrder) => new FuturesOrderDbEntity
+    {
+        BinanceID = futuresOrder.Id,
+        CreateTime = futuresOrder.CreateTime,
+        OrderSide = futuresOrder.Side,
+        OrderType = futuresOrder.Type,
+        Price = futuresOrder.Price,
+        Quantity = futuresOrder.Quantity,
+    };
+}
