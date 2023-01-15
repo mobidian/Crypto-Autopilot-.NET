9e2329725680cfc41ff7fbfe4de817c3fd199db6
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-15 03:32:54 +0200

    Added a logger adapter and datetime provider implementation

---
+++ Source/Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces.Logging;
+
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Logging;
+
+public class LoggerAdapter<TType> : ILoggerAdapter<TType>
+{
+    private readonly ILogger<TType> Logger;
+
+    public LoggerAdapter(ILogger<TType> logger) => this.Logger = logger;
+
+
+    public void LogInformation(string? message, params object?[] args) => this.Logger.LogInformation(message, args);
+
+    public void LogError(Exception? exception, string? message, params object?[] args) => this.Logger.LogError(exception, message, args);
+}
---
+++ Source/Infrastructure/Services/General/DateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿using Application.Interfaces.Services.General;
+
+namespace Infrastructure.Services.General;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime Now => DateTime.Now;
+
+    public DateTime UtcNow => DateTime.UtcNow;
+}
