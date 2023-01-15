b8cacfb66df56f12f0e6b7258581d6831b1de735
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-15 20:08:18 +0200

    Added another 2 integration tests

--- Tests/Infrastructure.Tests.Integration/FuturesTradesDBServiceClass/FuturesTradesDBServiceAddFuturesOrderTests.cs
+++ Tests/Infrastructure.Tests.Integration/FuturesTradesDBServiceClass/FuturesTradesDBServiceAddFuturesOrderTests.cs
@@ -4,6 +4,9 @@ using FluentAssertions;
 
 using Infrastructure.Tests.Integration.FuturesTradesDBServiceClass.Common;
 
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
 namespace Infrastructure.Tests.Integration.FuturesTradesDBServiceClass;
 
 public class FuturesTradesDBServiceAddFuturesOrderTests : FuturesTradesDBServiceTestsBase
@@ -45,5 +48,50 @@ public class FuturesTradesDBServiceAddFuturesOrderTests : FuturesTradesDBService
         var addedEntity = this.dbContext.FuturesOrders.Single();
         base.AssertAgainstAddedEntityAuditRecords(addedEntity);
         addedEntity.ToDomainObject().Should().BeEquivalentTo(futuresorder);
+    }
+
+    [Test, Order(3)]
+    public async Task AddFuturesOrderAsync_ShouldThrow_WhenFuturesOrderAlreadyExists()
+    {
+        // Arrange
+        var candlestick = this.CandlestickGenerator.Generate();
+        var futuresorder = this.FuturesOrderGenerator.Generate();
+
+        // Act
+        await this.SUT.AddFuturesOrderAsync(futuresorder, candlestick);
+        var func = async () => await this.SUT.AddFuturesOrderAsync(futuresorder, candlestick);
+        
+        // Assert
+        // Assert
+        (await func.Should().ThrowExactlyAsync<DbUpdateException>())
+            .WithMessage("An error occurred while saving the entity changes. See the inner exception for details.")
+            .WithInnerExceptionExactly<SqlException>()
+            .WithMessage($"Cannot insert duplicate key row in object 'dbo.FuturesOrders' with unique index 'IX_FuturesOrders_Binance ID'. The duplicate key value is ({futuresorder.Id}).");
+    }
+
+    [Test, Order(4)]
+    public async Task AddFuturesOrderAsync_ShouldThrow_WhenCandlestickIsNull()
+    {
+        // Arrange
+        var futuresorder = this.FuturesOrderGenerator.Generate();
+
+        // Act
+        var func = async () => await this.SUT.AddFuturesOrderAsync(futuresorder, null!);
+
+        // Assert
+        (await func.Should().ThrowExactlyAsync<ArgumentNullException>()).WithMessage("Value cannot be null. (Parameter 'Candlestick')");
+    }
+
+    [Test, Order(5)]
+    public async Task AddFuturesOrderAsync_ShouldThrow_WhenFuturesOrderIsNull()
+    {
+        // Arrange
+        var candlestick = this.CandlestickGenerator.Generate();
+        
+        // Act
+        var func = async () => await this.SUT.AddFuturesOrderAsync(null!, candlestick);
+
+        // Assert
+        (await func.Should().ThrowExactlyAsync<ArgumentNullException>()).WithMessage("Value cannot be null. (Parameter 'FuturesOrder')");
     }
 }
