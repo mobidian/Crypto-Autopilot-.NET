11b366ab19b5af5285d13bf78a958a8a419ed2ad
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-15 03:36:30 +0200

    Added more constraints and column names

--- Source/Infrastructure/Database/Configurations/CandlestickDbEntityConfiguration.cs
+++ Source/Infrastructure/Database/Configurations/CandlestickDbEntityConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Infrastructure.Database.Entities;
+﻿using Infrastructure.Database.Entities;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -15,14 +9,17 @@ public class CandlestickDbEntityConfiguration : IEntityTypeConfiguration<Candles
 {
     public void Configure(EntityTypeBuilder<CandlestickDbEntity> builder)
     {
-        builder.Property(x => x.CurrencyPair).HasMaxLength(8);
+        builder.Property(x => x.RecordCreatedDate).HasMaxLength(50).HasColumnName("Record Created Date");
+        builder.Property(x => x.RecordModifiedDate).HasMaxLength(50).HasColumnName("Record Modified Date");
+
+
+        builder.Property(x => x.CurrencyPair).HasMaxLength(8).HasColumnName("Currency Pair");
         builder.Property(x => x.Open).HasPrecision(18, 4);
         builder.Property(x => x.High).HasPrecision(18, 4);
         builder.Property(x => x.Low).HasPrecision(18, 4);
         builder.Property(x => x.Close).HasPrecision(18, 4);
 
         builder.HasIndex(x => x.CurrencyPair);
-
         builder.HasIndex(x => new { x.CurrencyPair, x.DateTime }).IsUnique();
     }
 }
--- Source/Infrastructure/Database/Configurations/FuturesOrderDbEntityConfiguration.cs
+++ Source/Infrastructure/Database/Configurations/FuturesOrderDbEntityConfiguration.cs
@@ -9,8 +9,15 @@ public class FuturesOrderDbEntityConfiguration : IEntityTypeConfiguration<Future
 {
     public void Configure(EntityTypeBuilder<FuturesOrderDbEntity> builder)
     {
-        builder.Property(x => x.OrderSide).HasMaxLength(8);
-        builder.Property(x => x.OrderType).HasMaxLength(32);
+        builder.Property(x => x.RecordCreatedDate).HasMaxLength(50).HasColumnName("Record Created Date");
+        builder.Property(x => x.RecordModifiedDate).HasMaxLength(50).HasColumnName("Record Modified Date");
+
+
+        builder.Property(x => x.CandlestickId).HasColumnName("Candlestick Id");
+        builder.Property(x => x.BinanceID).HasColumnName("Binance ID");
+        builder.Property(x => x.OrderSide).HasMaxLength(8).HasColumnName("Order Side");
+        builder.Property(x => x.OrderType).HasMaxLength(32).HasColumnName("Order Type");
+        
         builder.Property(x => x.Price).HasPrecision(18, 4);
         builder.Property(x => x.Quantity).HasPrecision(18, 4);
 
