bba48087daddac9140f4d52239f88c0b03b28504
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-15 17:18:35 +0200

    Added integration tests for FuturesTradesDBService.AddCandlestickAsync

---
+++ Tests/Infrastructure.Tests.Integration/FuturesTradesDBServiceClass/Common/FuturesTradesDBServiceTestsBase.cs
@@ -0,0 +1,66 @@
+﻿using Application.Interfaces.Services.General;
+
+using Binance.Net.Enums;
+using Binance.Net.Objects.Models.Futures;
+
+using Bogus;
+
+using Domain.Models;
+
+using Infrastructure.Database.Contexts;
+using Infrastructure.Services.Trading;
+
+using NSubstitute;
+
+using Respawn;
+
+namespace Infrastructure.Tests.Integration.FuturesTradesDBServiceClass.Common;
+
+public abstract class FuturesTradesDBServiceTestsBase
+{
+    protected const string ConnectionString = """Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=CryptoPilotTradesTestDatabase""";
+
+    protected FuturesTradesDBService SUT;
+    protected FuturesTradingDbContext dbContext;
+    protected IDateTimeProvider DateTimeProvider = Substitute.For<IDateTimeProvider>();
+    
+    protected readonly Faker<Candlestick> CandlestickGenerator = new Faker<Candlestick>()
+        .RuleFor(x => x.CurrencyPair, f => new CurrencyPair(f.Finance.Currency().Code, f.Finance.Currency().Code))
+        .RuleFor(c => c.Date, f => f.Date.Recent(365))
+        .RuleFor(c => c.Open, f => f.Random.Decimal(1000, 1500))
+        .RuleFor(c => c.High, (f, c) => f.Random.Decimal(c.Open, c.Open + 100))
+        .RuleFor(c => c.Low, (f, c) => f.Random.Decimal(c.Open - 100, c.Open))
+        .RuleFor(c => c.Close, (f, c) => f.Random.Decimal(1000, 1500))
+        .RuleFor(c => c.Volume, f => f.Random.Decimal(100000, 300000));
+    
+    private readonly Faker<BinanceFuturesOrder> FuturesOrderGenerator = new Faker<BinanceFuturesOrder>()
+        .RuleFor(o => o.Id, f => f.Random.Long())
+        .RuleFor(o => o.CreateTime, f => f.Date.Recent(365))
+        .RuleFor(o => o.Side, f => f.Random.Enum<OrderSide>())
+        .RuleFor(o => o.Type, f => f.Random.Enum<FuturesOrderType>())
+        .RuleFor(o => o.Price, f => f.Random.Decimal(0, 1000))
+        .RuleFor(o => o.Quantity, f => f.Random.Decimal(0, 10));
+
+
+    private Respawner DbRespawner;
+    protected async Task ClearDatabaseAsync() => await this.DbRespawner.ResetAsync(ConnectionString);
+
+
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        this.dbContext = new FuturesTradingDbContext(ConnectionString, this.DateTimeProvider);
+        await this.dbContext.Database.EnsureCreatedAsync();
+        
+        this.SUT = new FuturesTradesDBService(this.dbContext);
+
+        this.DbRespawner = await Respawner.CreateAsync(ConnectionString, new RespawnerOptions { CheckTemporalTables = true });
+    }
+    
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await this.dbContext.Database.EnsureDeletedAsync();
+    }
+}
---
+++ Tests/Infrastructure.Tests.Integration/FuturesTradesDBServiceClass/FuturesTradesDBServiceAddCandlestickTests.cs
@@ -0,0 +1,42 @@
+﻿using Application.Mapping;
+
+using Domain.Models;
+
+using FluentAssertions;
+
+using Infrastructure.Database.Contexts;
+using Infrastructure.Services.Trading;
+using Infrastructure.Tests.Integration.FuturesTradesDBServiceClass.Common;
+
+namespace Infrastructure.Tests.Integration.FuturesTradesDBServiceClass;
+
+public class FuturesTradesDBServiceAddCandlestickTests : FuturesTradesDBServiceTestsBase
+{
+    [TearDown]
+    public async Task TearDown() => await this.ClearDatabaseAsync();
+    
+    
+
+    [Test]
+    public async Task AddCandlestickAsync_ShouldAddCandlestick_WhenCandlestickIsValid()
+    {
+        // Arrange
+        var candlestick = this.CandlestickGenerator.Generate();
+        
+        // Act
+        await this.SUT.AddCandlestickAsync(candlestick);
+        
+        // Assert
+        this.dbContext.Candlesticks.Single().ToDomainObject().Should().BeEquivalentTo(candlestick);
+    }
+
+    [Test]
+    public async Task AddCandlestickAsync_ShouldThrow_WhenCandlestickIsNull()
+    {
+        // Act
+        var func = async () => await this.SUT.AddCandlestickAsync(null!);
+
+        // Assert
+        await func.Should().ThrowAsync<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'Candlestick')");
+    }
+}
