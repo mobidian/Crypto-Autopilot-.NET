9422ff125e67b3ab52b6caa3ce2f3be6402189ed
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-15 03:38:02 +0200

    Refactored the implementation of the FuturesTradingDbContext

--- Source/Infrastructure/Database/FuturesTradingDbContext.cs
+++ Source/Infrastructure/Database/FuturesTradingDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Interfaces.Services.General;
 
 using Infrastructure.Database.Entities;
+using Infrastructure.Services.General;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -11,24 +12,23 @@ public class FuturesTradingDbContext : DbContext
     private readonly string ConnectionString;
     private readonly IDateTimeProvider DateTimeProvider;
 
+    public FuturesTradingDbContext() : this("""Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=CryptoPilotTrades""", new DateTimeProvider()) { }
     public FuturesTradingDbContext(string connectionString, IDateTimeProvider dateTimeProvider)
     {
         this.ConnectionString = connectionString;
         this.DateTimeProvider = dateTimeProvider;
     }
-
-
+    
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        // optionsBuilder.UseSqlServer(this.ConnectionString);
-        optionsBuilder.UseSqlServer("""Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=CryptoPilotTrades""");
+        optionsBuilder.UseSqlServer(this.ConnectionString);
     }
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(FuturesTradingDbContext).Assembly, type => !type.IsInterface && !type.IsAbstract);
     }
-
 
     public DbSet<CandlestickDbEntity> Candlesticks { get; set; }
     public DbSet<FuturesOrderDbEntity> FuturesOrders { get; set; }
