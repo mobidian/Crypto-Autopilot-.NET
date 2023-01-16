0f97e3c7330ea411c1648620be84fe8ba3dd5fe5
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-16 16:00:46 +0200

    The IChartDataService<T> interface was in violation of the interface segregation principle so it has been split into 2 interfaces: IChartDataProvider for providing data, IChartDataService to interact with the data

---
+++ Source/Application/Interfaces/Services/Trading/IChartDataProvider.cs
@@ -0,0 +1,27 @@
+﻿using Skender.Stock.Indicators;
+
+namespace Application.Interfaces.Services.Trading;
+
+/// <summary>
+/// A provider that provides market data and maps the candlesticks as a specified generic type
+/// </summary>
+/// <typeparam name="T">The type that the candlesticks will be mapped as</typeparam>
+public interface IChartDataProvider<T> : IDisposable where T : IQuote
+{
+    /// <summary>
+    /// Gets all of the completed candlesticks registered by the provider
+    /// </summary>
+    public T[] Candlesticks { get; }
+
+    /// <summary>
+    /// Gets the unfinished candlestick in the chart asynchronously
+    /// </summary>
+    /// <returns></returns>
+    public Task<T> GetUnfinishedCandlestickAsync();
+    
+    /// <summary>
+    /// Gets the last finised candlestick in the chart asynchronously
+    /// </summary>
+    /// <returns></returns>
+    public Task<T> GetLastFinishedCandlestickAsync();
+}
--- Source/Application/Interfaces/Services/Trading/IChartDataService.cs
+++ Source/Application/Interfaces/Services/Trading/IChartDataService.cs
@@ -1,29 +1,16 @@
-﻿using Domain.Models;
-
-using Skender.Stock.Indicators;
+﻿using Skender.Stock.Indicators;
 
 namespace Application.Interfaces.Services.Trading;
 
 /// <summary>
-/// A service that provides market data and maps the candlesticks as a specified generic type
+/// A service that interacts with the provided market data
 /// </summary>
-/// <typeparam name="T">The type that the candlesticks will be mapped as</typeparam>
+/// <typeparam name="T"></typeparam>
 public interface IChartDataService<T> : IDisposable where T : IQuote
 {
-    /// <summary>
-    /// Gets all of the completed candlesticks
-    /// </summary>
-    public T[] Candlesticks { get; }
-
     /// <summary>
     /// Starts and returns the task of waiting for the current work in progress candle to be completed
     /// </summary>
     /// <returns></returns>
     public Task<T> WaitForNextCandleAsync();
-
-    public Task<T> GetUnfinishedCandlestickAsync();
-    
-    public Task<T> GetLastFinishedCandlestickAsync();
-
-    public Task RegisterAllCandlesticksAsync();
 }
