64974fba0b544440c0bccc281f1a20962d11ad2b
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-18 03:41:51 +0200

    Restyled the API folder structure, added extension methods, and created the IEndpoints interface to assist with assembly scanning for automation for service registration automation

---
+++ Source/Presentation.Api/Endpoints/GeneralEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using Presentation.Api.Endpoints.Internal;
+
+namespace Presentation.Api.Endpoints;
+
+public class GeneralEndpoints : IEndpoints
+{
+    public static void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+        
+    }
+
+    public static void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet("get", () =>
+        {
+
+        });
+    }
+}
---
+++ Source/Presentation.Api/Endpoints/MyStrategyEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Services.Trading.Strategy;
+
+using Presentation.Api.Endpoints.Internal;
+
+namespace Presentation.Api.Endpoints;
+
+public class MyStrategyEndpoints : IEndpoints
+{
+    public static void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<MyStrategyEngine>();
+    }
+
+    public static void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapPost("mysignal", (MyStrategyEngine engine) =>
+        {
+            
+        });
+    }
+}
