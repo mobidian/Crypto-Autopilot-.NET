a0b784e9a5061d4b5735fc84b96a42544e8dac12
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-19 21:54:52 +0200

    Added a dependency injection validation test

---
+++ Source/Presentation.Api/IApiMarker.cs
@@ -0,0 +1,5 @@
+﻿namespace Presentation.Api;
+
+public interface IApiMarker
+{
+}
---
+++ Tests/Presentation.Api.Tests.Integration/DependencyValidationTests/DependencyRegistrationTests.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+
+using Application.Interfaces.Logging;
+using Application.Interfaces.Services.General;
+using Application.Interfaces.Services.Trading;
+
+using Binance.Net.Clients;
+using Binance.Net.Clients.UsdFuturesApi;
+using Binance.Net.Enums;
+using Binance.Net.Interfaces.Clients;
+using Binance.Net.Interfaces.Clients.UsdFuturesApi;
+
+using CryptoExchange.Net.Authentication;
+
+using Domain.Models;
+
+using Infrastructure.Database.Contexts;
+using Infrastructure.Logging;
+using Infrastructure.Services.General;
+using Infrastructure.Services.Trading;
+using Infrastructure.Strategies.SimpleStrategy;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation.Api.Tests.Integration.DependencyValidationTests;
+
+public class DependencyRegistrationTests
+{
+    private readonly List<(Type? serviceType, Type? implementationType, ServiceLifetime? lifetime)> RequiredDescriptors = new()
+    {
+        (typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>), ServiceLifetime.Singleton),
+        (typeof(IDateTimeProvider), typeof(DateTimeProvider), ServiceLifetime.Singleton),
+
+        (typeof(FuturesTradingDbContext), typeof(FuturesTradingDbContext), ServiceLifetime.Singleton),
+        (typeof(IFuturesTradesDBService), typeof(FuturesTradesDBService), ServiceLifetime.Singleton),
+
+
+        (typeof(CurrencyPair), typeof(CurrencyPair), ServiceLifetime.Transient),
+        (typeof(ApiCredentials), typeof(ApiCredentials), ServiceLifetime.Transient),
+        (typeof(KlineInterval), typeof(KlineInterval), ServiceLifetime.Singleton),
+
+        (typeof(IBinanceClient), typeof(BinanceClient), ServiceLifetime.Transient),
+        (typeof(IBinanceSocketClient), typeof(BinanceSocketClient), ServiceLifetime.Transient),
+
+        (typeof(ICfdMarketDataProvider), typeof(BinanceCfdMarketDataProvider), ServiceLifetime.Singleton),
+        (typeof(ICfdTradingService), typeof(BinanceCfdTradingService), ServiceLifetime.Singleton),
+        
+        (typeof(IBinanceSocketClientUsdFuturesStreams), typeof(BinanceSocketClientUsdFuturesStreams), ServiceLifetime.Singleton),
+        (typeof(IFuturesMarketsCandlestickAwaiter), typeof(FuturesMarketsCandlestickAwaiter), ServiceLifetime.Singleton),
+
+
+        (typeof(SimpleStrategyEngine), typeof(SimpleStrategyEngine), ServiceLifetime.Singleton),
+    };
+
+
+    [Test]
+    public void RegistrationValidation()
+    {
+        var app = new WebApplicationFactory<IApiMarker>()
+            .WithWebHostBuilder(builder =>
+            builder.ConfigureServices(serviceCollection =>
+            {
+                var services = serviceCollection.ToList();
+                var res = ValidateServices(services);
+
+                if (!res.Success)
+                    Assert.Fail(res.Message);
+
+                Assert.Pass();
+            }));
+        
+        app.CreateClient(); // triggers the building of the application
+    }
+    private (bool Success, string Message) ValidateServices(List<ServiceDescriptor> serviceDescriptors)
+    {
+        var searchFailed = false;
+        var failedText = new StringBuilder();
+        var failedText2 = new StringBuilder();
+        
+        foreach (var requiredDescriptor in this.RequiredDescriptors)
+        {
+            var match = serviceDescriptors.SingleOrDefault(serviceDescriptor => 
+                serviceDescriptor.ServiceType == requiredDescriptor.serviceType &&
+                (serviceDescriptor.ImplementationType == requiredDescriptor.implementationType || serviceDescriptor.ImplementationFactory is not null || serviceDescriptor.ImplementationInstance is not null) &&
+                serviceDescriptor.Lifetime == requiredDescriptor.lifetime);
+            
+            if (match is not null)
+                continue;
+            
+            if (!searchFailed)
+            {
+                failedText.AppendLine("Did not find registered service for:");
+                searchFailed = true;
+            }
+            
+            failedText.AppendLine($"{requiredDescriptor.serviceType} | {requiredDescriptor.implementationType} | {requiredDescriptor.lifetime}");
+        }
+        
+        return (!searchFailed, failedText.ToString());
+    }
+}
