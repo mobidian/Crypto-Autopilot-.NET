8c68dceb424afecb8e2f28b9dff82dd432612ddb
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-20 00:54:23 +0200

    Added migrations for property configurations for the new properties of FuturesOrderDbEntity

--- Source/Infrastructure/Database/Migrations/20230119224258_AddAndConfigureMorePropertiesForFuturesOrderDbEntityMigration.Designer.cs
+++ Source/Infrastructure/Database/Migrations/20230119224258_AddAndConfigureMorePropertiesForFuturesOrderDbEntityMigration.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Database.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(FuturesTradingDbContext))]
-    partial class FuturesTradingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230119224258_AddAndConfigureMorePropertiesForFuturesOrderDbEntityMigration")]
+    partial class AddAndConfigureMorePropertiesForFuturesOrderDbEntityMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -175,7 +178,7 @@ namespace Infrastructure.Migrations
                     b.HasOne("Application.Data.Entities.CandlestickDbEntity", "Candlestick")
                         .WithMany("FuturesOrders")
                         .HasForeignKey("CandlestickId")
-                        .OnDelete(DeleteBehavior.Restrict)
+                        .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
                     b.Navigation("Candlestick");
---
+++ Source/Infrastructure/Database/Migrations/20230119224258_AddAndConfigureMorePropertiesForFuturesOrderDbEntityMigration.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAndConfigureMorePropertiesForFuturesOrderDbEntityMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AvgPrice",
+                table: "FuturesOrders",
+                type: "decimal(18,4)",
+                precision: 18,
+                scale: 4,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Order Status",
+                table: "FuturesOrders",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Order Working Type",
+                table: "FuturesOrders",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PriceProtect",
+                table: "FuturesOrders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "StopPrice",
+                table: "FuturesOrders",
+                type: "decimal(18,4)",
+                precision: 18,
+                scale: 4,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Time in force",
+                table: "FuturesOrders",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateTime",
+                table: "FuturesOrders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvgPrice",
+                table: "FuturesOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Order Status",
+                table: "FuturesOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Order Working Type",
+                table: "FuturesOrders");
+
+            migrationBuilder.DropColumn(
+                name: "PriceProtect",
+                table: "FuturesOrders");
+
+            migrationBuilder.DropColumn(
+                name: "StopPrice",
+                table: "FuturesOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Time in force",
+                table: "FuturesOrders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateTime",
+                table: "FuturesOrders");
+        }
+    }
+}
