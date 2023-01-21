fac1607cf568d180d41ed1beaa89a1a3431a29f0
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-21 21:39:27 +0200

    Extracted base abstract class SimpleStrategyEngine for simple trading strategies for long and short positions + fixed bug in SimpleLongStrategyEngine.MakeMoveAsync()

--- Source/Infrastructure/Strategies/SimpleStrategy/SimpleShortStrategyEngine.cs
+++ Source/Infrastructure/Strategies/SimpleStrategy/SimpleShortStrategyEngine.cs
@@ -1,5 +1,4 @@
 ﻿using Application.Interfaces.Services.Trading;
-using Application.Interfaces.Services.Trading.Strategy;
 
 using Binance.Net.Enums;
 
@@ -7,41 +6,40 @@ using Domain.Models;
 
 using Infrastructure.Notifications;
 using Infrastructure.Services.Trading;
-using Infrastructure.Strategies.Abstract;
 
 using MediatR;
 
 namespace Infrastructure.Strategies.SimpleStrategy;
 
 /// <summary>
-/// A simple strategy that opens a long position when <see cref="CFDMovingUp"/> is called and closes the position if it exists when <see cref="CFDMovingDown"/> is called
+/// A simple strategy that opens a short position when <see cref="CFDMovingDown"/> is called and closes the position if it exists when <see cref="CFDMovingUp"/> is called
 /// </summary>
-public sealed class SimpleLongStrategyEngine : SimpleStrategyEngine
+public sealed class SimpleShortStrategyEngine : SimpleStrategyEngine
 {
-    public SimpleLongStrategyEngine(CurrencyPair currencyPair, KlineInterval klineInterval, ICfdTradingService futuresTrader, ICfdMarketDataProvider futuresDataProvider, IFuturesMarketsCandlestickAwaiter candlestickAwaiter, IMediator mediator) : base(currencyPair, klineInterval, futuresTrader, futuresDataProvider, candlestickAwaiter, mediator) { }
-    
+    public SimpleShortStrategyEngine(CurrencyPair currencyPair, KlineInterval klineInterval, ICfdTradingService futuresTrader, ICfdMarketDataProvider futuresDataProvider, IFuturesMarketsCandlestickAwaiter candlestickAwaiter, IMediator mediator) : base(currencyPair, klineInterval, futuresTrader, futuresDataProvider, candlestickAwaiter, mediator) { }
+
     //// //// ////
-    
+
     internal override async Task MakeMoveAsync()
     {
         if (this.Signal is null)
             return;
-
-        if (this.Signal == TradingviewSignal.Up && !this.FuturesTrader.IsInPosition())
+        
+        if (this.Signal == TradingviewSignal.Down && !this.FuturesTrader.IsInPosition())
         {
-            await this.OpenLongPositionAsync();
+            await this.OpenShortPositionAsync();
         }
-        else if (this.Signal == TradingviewSignal.Down && this.FuturesTrader.IsInPosition())
+        else if (this.Signal == TradingviewSignal.Up && this.FuturesTrader.IsInPosition())
         {
             await this.ClosePositionAsync();
         }
 
         this.Signal = null;
     }
-    private async Task OpenLongPositionAsync()
+    private async Task OpenShortPositionAsync()
     {
         var price = await this.FuturesTrader.GetCurrentPriceAsync();
-        await this.FuturesTrader.OpenPositionAtMarketPriceAsync(OrderSide.Buy, 20, 0.99m * price, 1.01m * price);
+        await this.FuturesTrader.OpenPositionAtMarketPriceAsync(OrderSide.Sell, 20, 1.01m * price, 0.99m * price);
 
         var candlesticks = await this.FuturesDataProvider.GetCompletedCandlesticksAsync(this.KlineInterval);
         await this.Mediator.Publish(new PositionOpenedNotification(candlesticks.Last(), this.FuturesTrader.Position!));
---
+++ Source/Infrastructure/Strategies/SimpleStrategy/SimpleStrategyEngine.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces.Services.Trading;
+
+using Binance.Net.Enums;
+
+using Domain.Models;
+
+using Infrastructure.Services.Trading;
+using Infrastructure.Strategies.Abstract;
+
+using MediatR;
+
+namespace Infrastructure.Strategies.SimpleStrategy;
+
+public abstract class SimpleStrategyEngine : StrategyEngine
+{
+    protected SimpleStrategyEngine(CurrencyPair currencyPair, KlineInterval klineInterval, ICfdTradingService futuresTrader, ICfdMarketDataProvider futuresDataProvider, IFuturesMarketsCandlestickAwaiter candlestickAwaiter, IMediator mediator) : base(currencyPair, klineInterval, futuresTrader, futuresDataProvider, candlestickAwaiter, mediator) { }
+
+    
+    protected internal enum TradingviewSignal { Up, Down }
+    protected internal TradingviewSignal? Signal = null; // a null value indicates that the signal does not exist or has been consumed
+
+
+    public void CFDMovingUp() => Signal = TradingviewSignal.Up;
+    public void CFDMovingDown() => Signal = TradingviewSignal.Down;
+}
