0883f09e83066967d04a59f5049a2d8fb8a11f26
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-22 22:29:45 +0200

    Added a FuturesTradingDbContext factory for design time in favor of visual studio seeing the migrations in the Infrastructure project form the Presentation.Api project

---
+++ Source/Presentation.Api/Data/DesignTimeFuturesTradingDbContextDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Database.Contexts;
+using Infrastructure.Services.General;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Presentation.Api.Data;
+
+public class DesignTimeFuturesTradingDbContextDbContextFactory : IDesignTimeDbContextFactory<FuturesTradingDbContext>
+{
+    public FuturesTradingDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddUserSecrets<Program>()
+            .Build();
+
+        return new FuturesTradingDbContext(configuration.GetConnectionString("CryptoPilotTrades")!, new DateTimeProvider());
+    }
+}
