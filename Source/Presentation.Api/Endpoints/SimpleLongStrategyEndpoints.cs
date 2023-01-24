d27a6acc2389732beece9d892d16b3a56991a135
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-24 18:33:05 +0200

    Added endpoints for short and long simple strategies

---
+++ Source/Presentation.Api/Endpoints/SimpleLongStrategyEndpoints.cs
@@ -0,0 +1,41 @@
+﻿using Infrastructure.Strategies.SimpleStrategy;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Presentation.Api.Endpoints.Internal;
+
+namespace Presentation.Api.Endpoints;
+
+public class SimpleLongStrategyEndpoints : IEndpoints
+{
+    public static void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<SimpleLongStrategyEngine>();
+    }
+
+    public static void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        MapStartStopEndpoints(app);
+        MapStrategySignalsEndpoints(app);
+    }
+    private static void MapStartStopEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet("StartSimpleLongStrategy", async ([FromServices] SimpleLongStrategyEngine engine, IServiceProvider services) => await engine.TryAwaitStartupAsync(services, TimeSpan.FromSeconds(15)));
+
+        app.MapPost("StopSimpleLongStrategy", async ([FromServices] SimpleLongStrategyEngine engine, IServiceProvider services) => await engine.TryAwaitShutdownAsync(services, TimeSpan.FromSeconds(15)));
+    }
+    private static void MapStrategySignalsEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapPost("SimpleLongStrategyCfdUp", ([FromServices] SimpleLongStrategyEngine engine) =>
+        {
+            engine.CFDMovingUp();
+            return Results.Ok();
+        });
+
+        app.MapPost("SimpleLongStrategyCfdDown", ([FromServices] SimpleLongStrategyEngine engine) =>
+        {
+            engine.CFDMovingDown();
+            return Results.Ok();
+        });
+    }
+}
--- Source/Presentation.Api/Endpoints/SimpleShortStrategyEndpoints.cs
+++ Source/Presentation.Api/Endpoints/SimpleShortStrategyEndpoints.cs
@@ -6,13 +6,13 @@ using Presentation.Api.Endpoints.Internal;
 
 namespace Presentation.Api.Endpoints;
 
-public class SimpleStrategyEndpoints : IEndpoints
+public class SimpleShortStrategyEndpoints : IEndpoints
 {
     public static void AddServices(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddSingleton<SimpleLongStrategyEngine>();
+        services.AddSingleton<SimpleShortStrategyEngine>();
     }
-
+    
     public static void MapEndpoints(IEndpointRouteBuilder app)
     {
         MapStartStopEndpoints(app);
@@ -20,19 +20,19 @@ public class SimpleStrategyEndpoints : IEndpoints
     }
     private static void MapStartStopEndpoints(IEndpointRouteBuilder app)
     {
-        app.MapGet("StartSimpleStrategy", async ([FromServices] SimpleLongStrategyEngine engine, IServiceProvider services) => await engine.TryAwaitStartupAsync(services, TimeSpan.FromSeconds(15)));
+        app.MapGet("StartSimpleShortStrategy", async ([FromServices] SimpleShortStrategyEngine engine, IServiceProvider services) => await engine.TryAwaitStartupAsync(services, TimeSpan.FromSeconds(15)));
 
-        app.MapPost("StopSimpleStrategy", async ([FromServices] SimpleLongStrategyEngine engine, IServiceProvider services) => await engine.TryAwaitShutdownAsync(services, TimeSpan.FromSeconds(15)));
+        app.MapPost("StopSimpleShortStrategy", async ([FromServices] SimpleShortStrategyEngine engine, IServiceProvider services) => await engine.TryAwaitShutdownAsync(services, TimeSpan.FromSeconds(15)));
     }
     private static void MapStrategySignalsEndpoints(IEndpointRouteBuilder app)
     {
-        app.MapPost("CfdUp", ([FromServices] SimpleLongStrategyEngine engine) =>
+        app.MapPost("SimpleShortStrategyCfdUp", ([FromServices] SimpleShortStrategyEngine engine) =>
         {
             engine.CFDMovingUp();
             return Results.Ok();
         });
 
-        app.MapPost("CfdDown", ([FromServices] SimpleLongStrategyEngine engine) =>
+        app.MapPost("SimpleShortStrategyCfdDown", ([FromServices] SimpleShortStrategyEngine engine) =>
         {
             engine.CFDMovingDown();
             return Results.Ok();
