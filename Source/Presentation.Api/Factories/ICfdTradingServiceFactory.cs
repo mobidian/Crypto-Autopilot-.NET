76002a566f2b629bd4298ac9e7138d20bac87c0a
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-26 02:31:34 +0200

    Added factories for ICfdTradingService and IFuturesMarketsCandlestickAwaiter so they can be used in the DI container when initialising strategies

---
+++ Source/Presentation.Api/Factories/ICfdTradingServiceFactory.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces.Services.Trading;
+
+using Binance.Net.Interfaces.Clients;
+using Binance.Net.Interfaces.Clients.UsdFuturesApi;
+
+using Domain.Models;
+
+using Infrastructure.Services.Trading;
+
+namespace Presentation.Api.Factories;
+
+public class ICfdTradingServiceFactory
+{
+    public ICfdTradingService Create(CurrencyPair currencyPair, decimal leverage, IServiceProvider services)
+        => new BinanceCfdTradingService(
+            currencyPair,
+            leverage,
+            services.GetRequiredService<IBinanceClient>(),
+            services.GetRequiredService<IBinanceClientUsdFuturesApi>(),
+            services.GetRequiredService<IBinanceClientUsdFuturesApiTrading>(),
+            services.GetRequiredService<IBinanceClientUsdFuturesApiExchangeData>());
+}
---
+++ Source/Presentation.Api/Factories/IFuturesMarketsCandlestickAwaiterFactory.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces.Logging;
+using Application.Interfaces.Proxies;
+
+using Binance.Net.Enums;
+using Binance.Net.Interfaces.Clients.UsdFuturesApi;
+
+using Domain.Models;
+
+using Infrastructure.Services.Trading;
+
+namespace Presentation.Api.Factories;
+
+public class IFuturesMarketsCandlestickAwaiterFactory
+{
+    public IFuturesMarketsCandlestickAwaiter Create(CurrencyPair currencyPair, KlineInterval timeframe, IServiceProvider services)
+        => new FuturesMarketsCandlestickAwaiter(
+            currencyPair,
+            timeframe,
+            services.GetRequiredService<IBinanceSocketClientUsdFuturesStreams>(),
+            services.GetRequiredService<IUpdateSubscriptionProxy>(),
+            services.GetRequiredService<ILoggerAdapter<FuturesMarketsCandlestickAwaiter>>());
+}
