215c0cbae03faa50ae4784334231ed0178555ad8
MGithubber <mircea.ionescu3000@gmail.com>
2023-01-27 19:31:08 +0200

    Added a tracker for strategy engines

---
+++ Source/Presentation.Api/Services/Interfaces/IStrategiesTracker.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces.Services.Trading.Strategy;
+
+namespace Presentation.Api.Services.Interfaces;
+
+public interface IStrategiesTracker
+{
+    public int Count { get; }
+
+    public void Add(IStrategyEngine strategy);
+    public IEnumerable<IStrategyEngine> GetAll();
+    public IStrategyEngine? Get(Guid guid);
+    public void Remove(Guid guid);
+}
---
+++ Source/Presentation.Api/Services/StrategiesTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+
+using Application.Interfaces.Services.Trading.Strategy;
+
+using Presentation.Api.Services.Interfaces;
+
+namespace Presentation.Api.Services;
+
+public class StrategiesTracker : IStrategiesTracker
+{
+    private readonly ConcurrentDictionary<Guid, IStrategyEngine> Strategies = new();
+
+    public int Count => this.Strategies.Count;
+
+    public void Add(IStrategyEngine strategy) => this.Strategies.TryAdd(strategy.Guid, strategy);
+
+    public IEnumerable<IStrategyEngine> GetAll() => this.Strategies.Values;
+    public IStrategyEngine? Get(Guid guid) => this.Strategies.GetValueOrDefault(guid);
+
+    public void Remove(Guid guid) => this.Strategies.TryRemove(guid, out _);
+}
