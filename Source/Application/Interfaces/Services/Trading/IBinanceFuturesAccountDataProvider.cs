6a0ab33910dc5308927b4ba26cf9731f34b7c148
MGithubber <mircea.ionescu3000@gmail.com>
2023-03-02 01:21:21 +0200

    Added methods for getting USDⓈ-M Futures positions in the futures account data provider

--- Source/Application/Interfaces/Services/Trading/IBinanceFuturesAccountDataProvider.cs
+++ Source/Application/Interfaces/Services/Trading/IBinanceFuturesAccountDataProvider.cs
@@ -1,6 +1,15 @@
-﻿namespace Application.Interfaces.Services.Trading;
+﻿using Binance.Net.Enums;
+using Binance.Net.Objects.Models.Futures;
 
+namespace Application.Interfaces.Services.Trading;
+
+/// <summary>
+/// Provides data from a Binance USDⓈ-M Futures markets account
+/// </summary>
 public interface IBinanceFuturesAccountDataProvider
 {
     public Task<decimal> GetEquityAsync(string currencyPair);
+    
+    public Task<IEnumerable<BinancePositionDetailsUsdt>> GetPositionsAsync(string currencyPair);
+    public Task<IEnumerable<BinancePositionDetailsUsdt>> GetPositionAsync(string currencyPair, PositionSide positionSide);
 }
--- Source/Infrastructure/Services/Trading/BinanceFuturesAccountDataProvider.cs
+++ Source/Infrastructure/Services/Trading/BinanceFuturesAccountDataProvider.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Interfaces.Services.Trading;
 
+using Binance.Net.Enums;
 using Binance.Net.Interfaces.Clients.UsdFuturesApi;
+using Binance.Net.Objects.Models.Futures;
 
 using Infrastructure.Extensions;
 
@@ -22,5 +24,21 @@ public class BinanceFuturesAccountDataProvider : IBinanceFuturesAccountDataProvi
         
         var asset = callResult.Data.Assets.Where(binanceAsset => currencyPair.EndsWith(binanceAsset.Asset)).Single();
         return asset.AvailableBalance;
+    }
+
+    public async Task<IEnumerable<BinancePositionDetailsUsdt>> GetPositionsAsync(string currencyPair)
+    {
+        var callResult = await this.BinanceFuturesAccount.GetPositionInformationAsync(currencyPair);
+        callResult.ThrowIfHasError(); 
+        
+        return callResult.Data.Where(x => x.Symbol == currencyPair.ToUpperInvariant() && x.IsolatedMargin != 0);
+    }
+    
+    public async Task<IEnumerable<BinancePositionDetailsUsdt>> GetPositionAsync(string currencyPair, PositionSide positionSide)
+    {
+        var callResult = await this.BinanceFuturesAccount.GetPositionInformationAsync(currencyPair);
+        callResult.ThrowIfHasError();
+        
+        return callResult.Data.Where(x => x.Symbol == currencyPair.ToUpperInvariant() && x.PositionSide == positionSide && x.IsolatedMargin != 0);
     }
 }
