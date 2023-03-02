2b27d05790de79ffbc74599327394e8c6d91e1f8
MGithubber <mircea.ionescu3000@gmail.com>
2023-03-02 16:56:40 +0200

    Added more assertions to the position closing tests

--- Tests/Infrastructure.Tests.Integration/BinanceFuturesTradingServiceTestsBase/LongPositions/CloseLongPositionTests.cs
+++ Tests/Infrastructure.Tests.Integration/BinanceFuturesTradingServiceTestsBase/LongPositions/CloseLongPositionTests.cs
@@ -1,7 +1,5 @@
 ﻿using Binance.Net.Enums;
 
-using Infrastructure.Tests.Integration.BinanceFuturesTradingServiceTestsBase.Base;
-
 namespace Infrastructure.Tests.Integration.BinanceFuturesTradingServiceTestsBase.LongPositions;
 
 public class CloseLongPositionTests : Base.BinanceFuturesTradingServiceTestsBase
@@ -11,12 +9,29 @@ public class CloseLongPositionTests : Base.BinanceFuturesTradingServiceTestsBase
     {
         // Arrange
         var current_price = await this.MarketDataProvider.GetCurrentPriceAsync(this.CurrencyPair.Name);
-        await this.SUT.PlaceMarketOrderAsync(OrderSide.Buy, this.testMargin, 0.99m * current_price, 1.01m * current_price);
+        var orders = await this.SUT.PlaceMarketOrderAsync(OrderSide.Buy, this.testMargin, 0.99m * current_price, 1.01m * current_price);
+        
+        var ordersArray = orders.ToArray();
+        var entryOrder = ordersArray[0];
+        var stopLoss = ordersArray[1];
+        var takeProfit = ordersArray[1];
+
 
         // Act
         await this.SUT.ClosePositionAsync();
         
+
         // Assert
         this.SUT.IsInPosition().Should().BeFalse();
+
+        var longPosition = await this.AccountDataProvider.GetPositionAsync(this.CurrencyPair.Name, PositionSide.Long);
+        entryOrder = await this.MarketDataProvider.GetOrderAsync(entryOrder.Symbol, entryOrder.Id);
+        stopLoss = await this.MarketDataProvider.GetOrderAsync(stopLoss.Symbol, stopLoss.Id);
+        takeProfit = await this.MarketDataProvider.GetOrderAsync(takeProfit.Symbol, takeProfit.Id);
+        
+        longPosition.Should().BeNull();
+        entryOrder.Status.Should().Be(OrderStatus.Filled);         
+        stopLoss.Status.Should().Be(OrderStatus.Canceled);
+        takeProfit.Status.Should().Be(OrderStatus.Canceled);
     }
 }
--- Tests/Infrastructure.Tests.Integration/BinanceFuturesTradingServiceTestsBase/ShortPositions/CloseShortPositionTests.cs
+++ Tests/Infrastructure.Tests.Integration/BinanceFuturesTradingServiceTestsBase/ShortPositions/CloseShortPositionTests.cs
@@ -1,7 +1,5 @@
 ﻿using Binance.Net.Enums;
 
-using Infrastructure.Tests.Integration.BinanceFuturesTradingServiceTestsBase.Base;
-
 namespace Infrastructure.Tests.Integration.BinanceFuturesTradingServiceTestsBase.ShortPositions;
 
 public class CloseShortPositionTests : Base.BinanceFuturesTradingServiceTestsBase
@@ -11,12 +9,29 @@ public class CloseShortPositionTests : Base.BinanceFuturesTradingServiceTestsBas
     {
         // Arrange
         var current_price = await this.MarketDataProvider.GetCurrentPriceAsync(this.CurrencyPair.Name);
-        await this.SUT.PlaceMarketOrderAsync(OrderSide.Sell, this.testMargin, 1.01m * current_price, 0.99m * current_price);
+        var orders = await this.SUT.PlaceMarketOrderAsync(OrderSide.Sell, this.testMargin, 1.01m * current_price, 0.99m * current_price);
+
+        var ordersArray = orders.ToArray();
+        var entryOrder = ordersArray[0];
+        var stopLoss = ordersArray[1];
+        var takeProfit = ordersArray[1];
+
 
         // Act
         await this.SUT.ClosePositionAsync();
 
+
         // Assert
         this.SUT.IsInPosition().Should().BeFalse();
+
+        var shortPosition = await this.AccountDataProvider.GetPositionAsync(this.CurrencyPair.Name, PositionSide.Short);
+        entryOrder = await this.MarketDataProvider.GetOrderAsync(entryOrder.Symbol, entryOrder.Id);
+        stopLoss = await this.MarketDataProvider.GetOrderAsync(stopLoss.Symbol, stopLoss.Id);
+        takeProfit = await this.MarketDataProvider.GetOrderAsync(takeProfit.Symbol, takeProfit.Id);
+        
+        shortPosition.Should().BeNull();
+        entryOrder.Status.Should().Be(OrderStatus.Filled);
+        stopLoss.Status.Should().Be(OrderStatus.Canceled);
+        takeProfit.Status.Should().Be(OrderStatus.Canceled);
     }
 }
