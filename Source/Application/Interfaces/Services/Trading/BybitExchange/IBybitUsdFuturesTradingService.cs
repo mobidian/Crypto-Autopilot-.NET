1b9ead95f4112308b386d0cf8fa458758e283c6f
MGithubber <mircea.ionescu3000@gmail.com>
2023-03-05 22:27:12 +0200

    Added IBybitUsdFuturesTradingService defining methods for managing futures markets positions and limit orders + BybitUsdFuturesTradingService implementation

---
+++ Source/Application/Interfaces/Services/Trading/BybitExchange/IBybitUsdFuturesTradingService.cs
@@ -0,0 +1,23 @@
+﻿using Bybit.Net.Enums;
+using Bybit.Net.Objects.Models;
+
+namespace Application.Interfaces.Services.Trading.BybitExchange;
+
+public interface IBybitUsdFuturesTradingService
+{
+    public BybitPositionUsd? LongPosition { get; }
+    public BybitPositionUsd? ShortPosition { get; }
+    
+    public BybitUsdPerpetualOrder? BuyLimitOrder { get; }
+    public BybitUsdPerpetualOrder? SellLimitOrder { get; }
+
+    
+    public Task OpenPositionAsync(PositionSide positionSide, decimal Margin, decimal? StopLoss = null, decimal? TakeProfit = null, TriggerType tradingStopTriggerType = TriggerType.LastPrice);
+    public Task ModifyTradingStopAsync(PositionSide positionSide, decimal? newStopLoss = null, decimal? newTakeProfit = null, TriggerType newTradingStopTriggerType = TriggerType.LastPrice);
+    public Task ClosePositionAsync(PositionSide positionSide);
+    
+    
+    public Task PlaceLimitOrderAsync(OrderSide orderSide, decimal LimitPrice, decimal Margin, decimal? StopLoss = null, decimal? TakeProfit = null, TriggerType tradingStopTriggerType = TriggerType.LastPrice);
+    public Task ModifyLimitOrderAsync(OrderSide orderSide, decimal newLimitPrice, decimal newMargin, decimal? newStopLoss = null, decimal? newTakeProfit = null, TriggerType newTradingStopTriggerType = TriggerType.LastPrice);
+    public Task CancelLimitOrderAsync(OrderSide orderSide);
+}
