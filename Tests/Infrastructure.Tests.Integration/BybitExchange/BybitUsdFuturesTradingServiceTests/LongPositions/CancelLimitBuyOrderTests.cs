82cc74d9e156b2afd77a3b7a9a45cfb300c7cb3c
MGithubber <mircea.ionescu3000@gmail.com>
2023-03-06 00:52:32 +0200

    Added the remaining integration tests for limit order cancelling

---
+++ Tests/Infrastructure.Tests.Integration/BybitExchange/BybitUsdFuturesTradingServiceTests/LongPositions/CancelLimitBuyOrderTests.cs
@@ -0,0 +1,38 @@
+﻿using Application.Exceptions;
+
+using Bybit.Net.Enums;
+
+using Infrastructure.Tests.Integration.BybitExchange.BybitUsdFuturesTradingServiceTests.AbstractBase;
+
+namespace Infrastructure.Tests.Integration.BybitExchange.BybitUsdFuturesTradingServiceTests.LongPositions;
+
+public class CancelLimitBuyOrderTests : BybitUsdFuturesTradingServiceTestsBase
+{
+    [Test]
+    public async Task CancelLimitOrder_ShouldCancelLimitBuyOrder_WhenLimitBuyOrderExists()
+    {
+        // Arrange
+        var lastPrice = await this.FuturesDataProvider.GetLastPriceAsync(this.CurrencyPair.Name);
+        var limitPrice = lastPrice - 500;
+        var stopLoss = limitPrice - 300;
+        var takeProfit = limitPrice + 300;
+        var tradingStopTriggerType = TriggerType.LastPrice;
+        await this.SUT.PlaceLimitOrderAsync(OrderSide.Buy, limitPrice, this.Margin, stopLoss, takeProfit, tradingStopTriggerType);
+
+        // Act
+        await this.SUT.CancelLimitOrderAsync(OrderSide.Buy);
+        
+        // Assert
+        this.SUT.BuyLimitOrder.Should().BeNull();
+    }
+
+    [Test]
+    public async Task CancelLimitOrder_ShouldThrow_WhenLimitBuyOrderDoesNotExist()
+    {
+        // Act
+        var func = async () => await this.SUT.CancelLimitOrderAsync(OrderSide.Buy);
+        
+        // Assert
+        await func.Should().ThrowAsync<InvalidOrderException>().WithMessage("No open Buy limit order was found");
+    }
+}
