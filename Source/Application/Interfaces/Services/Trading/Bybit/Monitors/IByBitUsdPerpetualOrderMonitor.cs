966dc75c0d03dc1f080f736083aa5a150f04e4c3
MGithubber <mircea.ionescu3000@gmail.com>
2023-03-07 23:01:53 +0200

    Added monitor for Bybit usd perpetual orders

---
+++ Source/Application/Interfaces/Services/Trading/Bybit/Monitors/IByBitUsdPerpetualOrderMonitor.cs
@@ -0,0 +1,60 @@
+﻿using Application.Exceptions;
+
+using Bybit.Net.Enums;
+
+namespace Application.Interfaces.Services.Trading.Bybit.Monitors;
+
+/// <summary>
+/// Defines an interface for monitoring the status of ByBit USD perpetual orders
+/// </summary>
+public interface IByBitUsdPerpetualOrderMonitor
+{
+    /// <summary>
+    /// Gets a value indicating whether this order monitor is currently subscribed to order updates
+    /// </summary>
+    public bool Subscribed { get; }
+
+    
+    /// <summary>
+    /// Subscribes to order updates asynchronously
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation</returns>
+    public Task SubscribeToOrderUpdatesAsync();
+
+    /// <summary>
+    /// Unsubscribes from order updates asynchronously
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation</returns>
+    public Task UnsubscribeFromOrderUpdatesAsync();
+
+    /// <summary>
+    /// <para>Waits for the specified order to reach the specified status</para>
+    /// <para>If the order with the specified ID doesn't reach the specified status, the task will complete when cancellation is requested or the consumer unsubscribes from order updates</para>
+    /// <para>If the consumer unsubscribes a <see cref="NotSubscribedException"/> will be thrown</para>
+    /// <para>If cancellation is requested a <see cref="TaskCanceledException"/> will be thrown</para>
+    /// </summary>
+    /// <param name="orderID">The ID of the order to monitor</param>
+    /// <param name="orderStatus">The status of the order to wait for</param>
+    /// <param name="token">A cancellation token that can be used to cancel the operation</param>
+    /// <returns>A task that represents the asynchronous operation</returns>
+    /// <exception cref="NotSubscribedException"></exception>
+    /// <exception cref="TaskCanceledException"></exception>
+    public Task WaitForOrderToReachStatusAsync(Guid orderID, OrderStatus orderStatus, CancellationToken token = default);
+    
+    /// <summary>
+    /// <para>Waits for any of the specified orders to reach the specified status and returns the ID of the first order that reaches the specified status</para>
+    /// <para>If none of the specified orders reach the specified status, the task will complete when cancellation is requested or the consumer unsubscribes from order updates</para>
+    /// <para>If the consumer unsubscribes a <see cref="NotSubscribedException"/> will be thrown</para>
+    /// <para>If cancellation is requested a <see cref="TaskCanceledException"/> will be thrown</para>
+    /// </summary>
+    /// <param name="orderIDs">The IDs of the orders to monitor</param>
+    /// <param name="orderStatus">The status of the order to wait for</param>
+    /// <param name="token">A cancellation token that can be used to cancel the operation</param>
+    /// <returns>
+    /// <para>A task that represents the asynchronous operation</para>
+    /// <para>The task result is the ID of the first order that reaches the specified status</para>
+    /// </returns>
+    /// <exception cref="NotSubscribedException"></exception>
+    /// <exception cref="TaskCanceledException"></exception>
+    public Task<Guid> WaitForAnyOrderToReachStatusAsync(IEnumerable<Guid> orderIDs, OrderStatus orderStatus, CancellationToken token = default);
+}
---
+++ Source/Infrastructure/Services/Trading/Bybit/Monitors/ByBitUsdPerpetualOrderMonitor.cs
@@ -0,0 +1,100 @@
+﻿using Application.Exceptions;
+using Application.Interfaces.Logging;
+using Application.Interfaces.Proxies;
+using Application.Interfaces.Services.Trading.Bybit.Monitors;
+
+using Bybit.Net.Clients.UsdPerpetualApi;
+using Bybit.Net.Enums;
+using Bybit.Net.Interfaces.Clients.UsdPerpetualApi;
+using Bybit.Net.Objects.Models.Socket;
+
+using CryptoExchange.Net.Sockets;
+
+using Infrastructure.Extensions;
+
+namespace Infrastructure.Services.Trading.Bybit.Monitors;
+
+public class ByBitUsdPerpetualOrderMonitor : IByBitUsdPerpetualOrderMonitor
+{
+    private readonly IBybitSocketClientUsdPerpetualStreams UsdPerpetualStreams;
+    private readonly IUpdateSubscriptionProxy UsdPerpetualUpdatesSubscription;
+    private readonly ILoggerAdapter<ByBitUsdPerpetualOrderMonitor> Logger;
+
+    public ByBitUsdPerpetualOrderMonitor(IBybitSocketClientUsdPerpetualStreams usdPerpetualStreams, IUpdateSubscriptionProxy usdPerpetualUpdatesSubscription, ILoggerAdapter<ByBitUsdPerpetualOrderMonitor> logger) : this(usdPerpetualStreams, usdPerpetualUpdatesSubscription, logger, new Dictionary<Guid, OrderStatus?>()) { }
+    public ByBitUsdPerpetualOrderMonitor(IBybitSocketClientUsdPerpetualStreams usdPerpetualStreams, IUpdateSubscriptionProxy usdPerpetualUpdatesSubscription, ILoggerAdapter<ByBitUsdPerpetualOrderMonitor> logger, IDictionary<Guid, OrderStatus?> orders)
+    {
+        this.UsdPerpetualStreams = usdPerpetualStreams ?? throw new ArgumentNullException(nameof(usdPerpetualStreams));
+        this.UsdPerpetualUpdatesSubscription = usdPerpetualUpdatesSubscription ?? throw new ArgumentNullException(nameof(usdPerpetualUpdatesSubscription));
+        this.Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        this.Orders = orders ?? throw new ArgumentNullException(nameof(orders));
+    }
+    
+
+    
+    private readonly IDictionary<Guid, OrderStatus?> Orders;
+    public bool Subscribed { get; private set; }
+
+    public async Task SubscribeToOrderUpdatesAsync()
+    {
+        if (this.Subscribed)
+            return;
+
+
+        var callResult = await this.UsdPerpetualStreams.SubscribeToOrderUpdatesAsync(this.HandleUsdPerpetualOrderUpdate);
+        callResult.ThrowIfHasError("Could not subscribe to user USD Perpetual order updates");
+        
+        this.UsdPerpetualUpdatesSubscription.SetSubscription(callResult.Data);
+        this.Subscribed = true;
+    }
+    internal void HandleUsdPerpetualOrderUpdate(DataEvent<IEnumerable<BybitUsdPerpetualOrderUpdate>> dataEvent)
+    {
+        var perpetualOrder = dataEvent.Data.Last();
+        this.Orders[Guid.Parse(perpetualOrder.Id)] = perpetualOrder.Status;
+    }
+    
+    public async Task UnsubscribeFromOrderUpdatesAsync()
+    {
+        if (!this.Subscribed)
+            throw new NotSubscribedException("Not subscribed to perpetual order updates");
+
+        await this.UsdPerpetualStreams.UnsubscribeAsync(this.UsdPerpetualUpdatesSubscription.Id);
+        this.Subscribed = false;
+    }
+
+    public async Task WaitForOrderToReachStatusAsync(Guid orderID, OrderStatus orderStatus, CancellationToken token = default)
+    {
+        if (!this.Subscribed)
+            throw new NotSubscribedException("Not subscribed to perpetual order updates");
+
+        this.Orders.TryAdd(orderID, null);
+        
+        while (this.Orders[orderID] != orderStatus && this.Subscribed)
+            await Task.Delay(50, token);
+        
+        this.ThrowIfConsumerUnsubscribed(orderStatus);
+    }
+    public async Task<Guid> WaitForAnyOrderToReachStatusAsync(IEnumerable<Guid> orderIDs, OrderStatus orderStatus, CancellationToken token = default)
+    {
+        if (!this.Subscribed)
+            throw new NotSubscribedException("Not subscribed to perpetual order updates");
+
+        foreach (var orderId in orderIDs)
+            this.Orders.TryAdd(orderId, null);
+
+        while (!this.Orders.Values.Any(x => x == orderStatus) && this.Subscribed)
+            await Task.Delay(50, token);
+        
+        this.ThrowIfConsumerUnsubscribed(orderStatus);
+
+        return this.Orders.First(x => x.Value == orderStatus).Key;
+    }
+
+    private void ThrowIfConsumerUnsubscribed(OrderStatus orderStatus)
+    {
+        if (this.Subscribed)
+            return;
+        
+        var exceptionMessage = $"The operation of waiting for any of the orders with the specified IDs to reach {orderStatus} has been cancelled as a consequence of the consumer unsubscribing from order updates";
+        throw new TaskCanceledException(exceptionMessage, new NotSubscribedException("Not subscribed to perpetual order updates"));
+    }
+}
