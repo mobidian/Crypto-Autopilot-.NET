53ff7f4de29e9161a1da3dc6d706b752e6c0b1df
MGithubber <mircea.ionescu3000@gmail.com>
2023-03-07 16:38:49 +0200

    Added methods for all logging levels in the logger adapter

--- Source/Application/Interfaces/Logging/ILoggerAdapter.cs
+++ Source/Application/Interfaces/Logging/ILoggerAdapter.cs
@@ -1,8 +1,18 @@
-﻿namespace Application.Interfaces.Logging;
+﻿using Microsoft.Extensions.Logging;
+
+namespace Application.Interfaces.Logging;
 
 public interface ILoggerAdapter<TType>
 {
+    public void LogTrace(EventId eventId, Exception? exception, string? message, params object?[] args);
+
+    public void LogDebug(EventId eventId, Exception? exception, string? message, params object?[] args);
+
     public void LogInformation(string? message, params object?[] args);
-    
+
+    public void LogWarning(EventId eventId, Exception? exception, string? message, params object?[] args);
+
     public void LogError(Exception? exception, string? message, params object?[] args);
+
+    public void LogCritical(EventId eventId, Exception? exception, string? message, params object?[] args);
 }
--- Source/Infrastructure/Logging/LoggerAdapter.cs
+++ Source/Infrastructure/Logging/LoggerAdapter.cs
@@ -10,8 +10,16 @@ public class LoggerAdapter<TType> : ILoggerAdapter<TType>
 
     public LoggerAdapter(ILogger<TType> logger) => this.Logger = logger;
 
-
+    
+    public void LogTrace(EventId eventId, Exception? exception, string? message, params object?[] args) => this.Logger.LogTrace(eventId, exception, message, args);
+    
+    public void LogDebug(EventId eventId, Exception? exception, string? message, params object?[] args) => this.Logger.LogDebug(eventId, exception, message, args);
+    
     public void LogInformation(string? message, params object?[] args) => this.Logger.LogInformation(message, args);
 
+    public void LogWarning(EventId eventId, Exception? exception, string? message, params object?[] args) => this.Logger.LogWarning(eventId, exception, message, args);
+
     public void LogError(Exception? exception, string? message, params object?[] args) => this.Logger.LogError(exception, message, args);
+
+    public void LogCritical(EventId eventId, Exception? exception, string? message, params object?[] args) => this.Logger.LogCritical(eventId, exception, message, args);
 }
