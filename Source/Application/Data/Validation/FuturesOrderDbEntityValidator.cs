645a77839d3a6cf2b5da00eb27218e1e50c448d0
MGithubber <mircea.ionescu3000@gmail.com>
2023-04-12 01:18:34 +0300

    Added database entity validators

---
+++ Source/Application/Data/Validation/FuturesOrderDbEntityValidator.cs
@@ -0,0 +1,50 @@
+﻿using Application.Data.Entities;
+using Application.Extensions.Bybit;
+
+using Bybit.Net.Enums;
+
+using FluentValidation;
+
+namespace Application.Data.Validation;
+
+public class FuturesOrderDbEntityValidator : AbstractValidator<FuturesOrderDbEntity>
+{
+    public FuturesOrderDbEntityValidator()
+    {
+        this.RuleFor(order => order.BybitID).NotEqual(Guid.Empty);
+        this.RuleFor(order => order.CurrencyPair).NotEqual(string.Empty);
+        this.RuleFor(order => order.UpdateTime).GreaterThanOrEqualTo(order => order.CreateTime);
+        this.RuleFor(order => order.Price).GreaterThanOrEqualTo(0);
+        this.RuleFor(order => order.Quantity).GreaterThanOrEqualTo(0);
+        this.RuleFor(order => order.StopLoss).GreaterThanOrEqualTo(0);
+        this.RuleFor(order => order.TakeProfit).GreaterThanOrEqualTo(0);
+        
+        this.RuleFor(order => order.TimeInForce).Equal(TimeInForce.GoodTillCanceled).When(order => order.Type == OrderType.Limit);
+        this.RuleFor(order => order.TimeInForce).Equal(TimeInForce.ImmediateOrCancel).When(order => order.Type == OrderType.Market);
+
+        this.RuleFor(order => order.StopLoss).LessThan(order => order.Price).When(order => order.Side == OrderSide.Buy);
+        this.RuleFor(order => order.TakeProfit).GreaterThan(order => order.Price).When(order => order.Side == OrderSide.Buy);
+        
+        this.RuleFor(order => order.StopLoss).GreaterThan(order => order.Price).When(order => order.Side == OrderSide.Sell);
+        this.RuleFor(order => order.TakeProfit).LessThan(order => order.Price).When(order => order.Side == OrderSide.Sell);
+
+        
+        this.RuleSet("CheckFkRelationships", () =>
+        {
+            this.RuleFor(order => order)
+            .Must(order => order.PositionId is not null && order.Side.ToPositionSide() == order.PositionSide)
+            .When(order => order.Type == OrderType.Limit && order.Status == OrderStatus.Filled)
+            .WithMessage("A filled limit order must point to a position with the appropriate position side.");
+
+            this.RuleFor(order => order)
+                .Must(order => order.PositionId is not null)
+                .When(order => order.Type == OrderType.Market)
+                .WithMessage("A market order must point to a position.");
+
+            this.RuleFor(order => order)
+                .Must(order => order.PositionId is null)
+                .When(order => order.Type == OrderType.Limit && order.Status != OrderStatus.Filled)
+                .WithMessage("A limit order which has not been filled must not point to a position.");
+        });
+    }
+}
---
+++ Source/Application/Data/Validation/FuturesPositionDbEntityValidator.cs
@@ -0,0 +1,34 @@
+﻿using Application.Data.Entities;
+
+using Bybit.Net.Enums;
+
+using FluentValidation;
+
+namespace Application.Data.Validation;
+
+public class FuturesPositionDbEntityValidator : AbstractValidator<FuturesPositionDbEntity>
+{
+    public FuturesPositionDbEntityValidator()
+    {
+        this.RuleFor(position => position.CryptoAutopilotId).NotEqual(Guid.Empty);
+        this.RuleFor(position => position.CurrencyPair).NotEqual(string.Empty);
+        this.RuleFor(position => position.Margin).GreaterThanOrEqualTo(0);
+        this.RuleFor(position => position.Leverage).GreaterThanOrEqualTo(0);
+        this.RuleFor(position => position.Quantity).GreaterThanOrEqualTo(0);
+        this.RuleFor(position => position.EntryPrice).GreaterThanOrEqualTo(0);
+        this.RuleFor(position => position.ExitPrice).GreaterThanOrEqualTo(0).Unless(position => position.ExitPrice is null);
+        
+        this.RuleSet("CheckFkRelationships", () =>
+        {
+            this.RuleFor(position => position)
+            .Must(position =>
+            {
+                if (position.FuturesOrders is null)
+                    return false;
+
+                return position.FuturesOrders.Any(o => o.Type == OrderType.Market || (o.Type == OrderType.Limit && o.Status == OrderStatus.Filled));
+            })
+            .WithMessage("A position cannot exist in the database without an opening futures order pointing to it.");
+        });
+    }
+}
