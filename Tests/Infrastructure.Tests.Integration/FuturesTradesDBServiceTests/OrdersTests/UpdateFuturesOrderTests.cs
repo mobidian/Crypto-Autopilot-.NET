e94ac000f077e4fbc64c9b956dc93432508b6e67
MGithubber <mircea.ionescu3000@gmail.com>
2023-05-04 01:32:17 +0300

    Added a new integtation test for UpdateFuturesOrderAsync

--- Tests/Infrastructure.Tests.Integration/FuturesTradesDBServiceTests/OrdersTests/UpdateFuturesOrderTests.cs
+++ Tests/Infrastructure.Tests.Integration/FuturesTradesDBServiceTests/OrdersTests/UpdateFuturesOrderTests.cs
@@ -124,4 +124,31 @@ public class UpdateFuturesOrderTests : FuturesTradesDBServiceTestsBase
                 .WithInnerExceptionExactly<FluentValidation.ValidationException>()
                 .And.Errors.Should().ContainSingle(error => error.ErrorMessage == "An order which points to a position must have the appropriate value for the PositionSide property.");
     }
+
+    
+    [Test]
+    public async Task UpdateFuturesOrderWithPositionGuid_ShouldThrow_WhenThePositionSideDoesNotMatchTheOldPositionSideAndItIsAlreadyPointingToSomePosition()
+    {
+        // Arrange
+        var position = this.FuturesPositionsGenerator.Generate($"default, {PositionSide.Buy.ToRuleSetName()}");
+        var orders = this.FuturesOrdersGenerator.Generate(10, $"default, {OrderType.Market.ToRuleSetName()}, {OrderSide.Buy.ToRuleSetName()}, {PositionSide.Buy.ToRuleSetName()}");
+        await InsertRelatedPositionAndOrdersAsync(position, orders);
+
+        var n = Random.Shared.Next(orders.Count);
+        var updatedOrder = this.FuturesOrdersGenerator.Clone()
+            .RuleFor(x => x.BybitID, orders[n].BybitID)
+            .Generate($"default, {OrderType.Market.ToRuleSetName()}, {OrderSide.Buy.ToRuleSetName()}, {PositionSide.Sell.ToRuleSetName()}");
+
+
+        // Act
+        var func = async () => await this.SUT.UpdateFuturesOrderAsync(updatedOrder.BybitID, updatedOrder);
+
+        
+        // Assert
+        (await func.Should()
+            .ThrowExactlyAsync<DbUpdateException>()
+            .WithMessage("An error occurred while validating relationships between entities. The database update operation cannot be performed."))
+                .WithInnerExceptionExactly<FluentValidation.ValidationException>()
+                .And.Errors.Should().ContainSingle(error => error.ErrorMessage == "An order which points to a position must have the appropriate value for the PositionSide property.");
+    }
 }
