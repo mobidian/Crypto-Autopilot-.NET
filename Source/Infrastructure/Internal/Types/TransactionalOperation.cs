0f0b6616a1408cd177e45b305410a456860c07f4
MGithubber <mircea.ionescu3000@gmail.com>
2023-05-21 04:40:13 +0300

    Updated the TransactionalOperation class

--- Source/Infrastructure/Internal/Types/TransactionalOperation.cs
+++ Source/Infrastructure/Internal/Types/TransactionalOperation.cs
@@ -2,14 +2,41 @@
 
 namespace Infrastructure.Internal.Types;
 
-internal class TransactionalOperation : IDisposable
+internal class TransactionalOperation : IDisposable, IAsyncDisposable
 {
     private readonly IDbContextTransaction Transaction;
     public TransactionalOperation(IDbContextTransaction transaction) => this.Transaction = transaction;
-
+    
     public void Dispose()
     {
-        this.Transaction.Commit();
-        this.Transaction.Dispose();
+        try
+        {
+            this.Transaction.Commit();
+        }
+        catch
+        {
+            this.Transaction.Rollback();
+            throw;
+        }
+        finally
+        {
+            this.Transaction.Dispose();
+        }
+    }    
+    public async ValueTask DisposeAsync()
+    {
+        try
+        {
+            await this.Transaction.CommitAsync();
+        }
+        catch
+        {
+            await this.Transaction.RollbackAsync();
+            throw;
+        }
+        finally
+        {
+            await this.Transaction.DisposeAsync();
+        }
     }
 }
