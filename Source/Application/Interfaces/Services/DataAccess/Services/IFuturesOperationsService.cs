2c0017e8f16bed0d63e58763e7567a1b3b2d828f
MGithubber <mircea.ionescu3000@gmail.com>
2023-05-21 04:35:09 +0300

    Added a service layer in the data access for the futures orders/positions

---
+++ Source/Application/Interfaces/Services/DataAccess/Services/IFuturesOperationsService.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models.Orders;
+
+namespace Application.Interfaces.Services.DataAccess.Services;
+
+public interface IFuturesOperationsService
+{
+    public Task AddFuturesPositionAndOrdersAsync(FuturesPosition position, IEnumerable<FuturesOrder> orders);
+    public Task UpdateFuturesPositionAndAddOrdersAsync(Guid cryptoAutopilotId, FuturesPosition updatedPosition, IEnumerable<FuturesOrder> newOrders);
+}
