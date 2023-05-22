b35ed2a9ac657b278ef3e39d3c48ba4e6eb706b5
MGithubber <mircea.ionescu3000@gmail.com>
2023-05-22 06:42:32 +0300

    Added a SignalInfoConverter type

---
+++ Source/Infrastructure/Database/ValueConverters/SignalInfoConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+using Domain.Converters;
+using Domain.Models.Signals;
+
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.Database.ValueConverters;
+
+public class SignalInfoConverter : ValueConverter<SignalInfo, string>
+{
+    internal SignalInfoConverter() : base(
+        v => SerializeToJson(JsonSignalnformationConverter.Empty, v),
+        v => DeserIalizeFromJson(JsonSignalnformationConverter.Empty, v))
+    {
+    }
+    public SignalInfoConverter(JsonSignalnformationConverter jsonConverter) : base(
+        v => SerializeToJson(jsonConverter, v),
+        v => DeserIalizeFromJson(jsonConverter, v))
+    {
+    }
+    
+    private static string SerializeToJson(JsonSignalnformationConverter jsonConverter, SignalInfo v)
+    {
+        var options = new JsonSerializerOptions { Converters = { jsonConverter } };
+        return JsonSerializer.Serialize(v, options);
+    }
+
+    private static SignalInfo DeserIalizeFromJson(JsonSignalnformationConverter jsonConverter, string v)
+    {
+        var options = new JsonSerializerOptions { Converters = { jsonConverter } };
+        return JsonSerializer.Deserialize<SignalInfo>(v, options)!;
+    }
+}
