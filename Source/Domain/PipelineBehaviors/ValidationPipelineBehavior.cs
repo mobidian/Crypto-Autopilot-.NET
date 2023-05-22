306e26d2677c7ce425800cd3017a6d269e902c51
MGithubber <mircea.ionescu3000@gmail.com>
2023-05-23 02:02:25 +0300

    Fixed duplicate validation in ValidationPipelineBehavior

--- Source/Domain/PipelineBehaviors/ValidationPipelineBehavior.cs
+++ Source/Domain/PipelineBehaviors/ValidationPipelineBehavior.cs
@@ -20,8 +20,9 @@ public class ValidationPipelineBehavior<TRequest, TResponse> : IPipelineBehavior
         var failures = this.Validators
             .Select(x => x.Validate(ctx))
             .SelectMany(x => x.Errors)
-            .Where(x => x is not null);
-        
+            .Where(x => x is not null)
+            .ToArray(); // by materializing the query the validators are only executed once, preventing duplication of error messages
+
         if (failures.Any())
         {
             throw new ValidationException(failures);
