c81e224d23cbf29e1bdf304c2b2289b7fc16b1c5
MGithubber <mircea.ionescu3000@gmail.com>
2023-05-23 03:45:38 +0300

    Added missing rule in CreatePositionCommandValidator

--- Source/Domain/Validation/Positions/CreatePositionCommandValidator.cs
+++ Source/Domain/Validation/Positions/CreatePositionCommandValidator.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Commands.Positions;
+﻿using Bybit.Net.Enums;
+
+using Domain.Commands.Positions;
 
 using FluentValidation;
 
@@ -6,16 +8,22 @@ namespace Domain.Validation.Positions;
 
 public class CreatePositionCommandValidator : AbstractValidator<CreatePositionCommand>
 {
+    private static readonly FuturesOrderValidator OrderValidator = new();
     private static readonly FuturesPositionValidator PositionValidator = new();
 
     public CreatePositionCommandValidator()
     {
+        this.RuleFor(command => command.FuturesOrders).Must(orders => orders.All(x => OrderValidator.Validate(x).IsValid));
         this.RuleFor(command => command.Position).SetValidator(PositionValidator);
-
+        
         this.RuleFor(command => command.Position).NotNull();
         
         this.RuleFor(command => command)
             .Must(command => command.FuturesOrders.All(order => order.PositionSide == command.Position.Side))
             .WithMessage("The position side must match the position side of the related orders.");
+        
+        this.RuleFor(command => command)
+            .Must(command => command.FuturesOrders.All(order => order.Type == OrderType.Market || (order.Type == OrderType.Limit && order.Status == OrderStatus.Filled)))
+            .WithMessage("A limit order which has not been filled must not point to a position.");
     }
 }
