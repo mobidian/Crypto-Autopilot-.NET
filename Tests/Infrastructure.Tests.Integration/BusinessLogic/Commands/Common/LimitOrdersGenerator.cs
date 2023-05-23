8da0b57c709e6d7491fd882ec6f75a75001fb142
MGithubber <mircea.ionescu3000@gmail.com>
2023-05-23 15:51:44 +0300

    Added 2 new classes for creating valid limit orders and position opening orders for integration testing

---
+++ Tests/Infrastructure.Tests.Integration/BusinessLogic/Commands/Common/LimitOrdersGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Bybit.Net.Enums;
+
+using Infrastructure.Tests.Integration.DataAccess.Extensions;
+
+namespace Infrastructure.Tests.Integration.BusinessLogic.Commands.Common;
+
+public class LimitOrdersGenerator : AbstractFuturesDataGenerator
+{
+    public override IEnumerator<object[]> GetEnumerator()
+    {
+        foreach (var positionSideRule in positionSideRules)
+        {
+            foreach (var orderSideRule in orderSideRules)
+            {
+                var order = this.FuturesOrdersGenerator.Generate($"default, {limitOrderNotFilledRule}, {orderSideRule}, {positionSideRule}");
+                yield return new object[] { order };
+            }
+        }
+    }
+}
---
+++ Tests/Infrastructure.Tests.Integration/BusinessLogic/Commands/Common/PositionOpeningOrdersGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Bybit.Net.Enums;
+
+using Infrastructure.Tests.Integration.DataAccess.Extensions;
+
+namespace Infrastructure.Tests.Integration.BusinessLogic.Commands.Common;
+
+public class PositionOpeningOrdersGenerator : AbstractFuturesDataGenerator
+{
+    public override IEnumerator<object[]> GetEnumerator()
+    {
+        foreach (var positionSideRule in positionSideRules)
+        {
+            foreach (var orderSideRule in orderSideRules)
+            {
+                var order1 = this.FuturesOrdersGenerator.Generate($"default, {marketOrderRule}, {orderSideRule}, {positionSideRule}");
+                var order2 = this.FuturesOrdersGenerator.Generate($"default, {limitFilledRule}, {orderSideRule}, {positionSideRule}");
+                
+                yield return new object[] { order1 };
+                yield return new object[] { order2 };
+            }
+        }
+    }
+}
