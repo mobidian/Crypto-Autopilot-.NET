19221bab94c159e31760fe5cf215b57959151f30
MGithubber <mircea.ionescu3000@gmail.com>
2023-05-23 15:20:39 +0300

    Renamed the CreateLimitOrderCommand and UpdateOrderCommand handlers

--- Source/Application/CommandHandlers/Orders/CreateLimitOrderCommandHandler.cs
+++ Source/Application/CommandHandlers/Orders/CreateLimitOrderCommandHandler.cs
@@ -6,10 +6,10 @@ using MediatR;
 
 namespace Application.CommandHandlers.LimitOrders;
 
-public class CreateLimitOrderInDatabaseCommandHandler : IRequestHandler<CreateLimitOrderCommand, Unit>
+public class CreateLimitOrderCommandHandler : IRequestHandler<CreateLimitOrderCommand, Unit>
 {
     private readonly IFuturesOrdersRepository OrdersRepository;
-    public CreateLimitOrderInDatabaseCommandHandler(IFuturesOrdersRepository ordersRepository) => this.OrdersRepository = ordersRepository;
+    public CreateLimitOrderCommandHandler(IFuturesOrdersRepository ordersRepository) => this.OrdersRepository = ordersRepository;
     
     public async Task<Unit> Handle(CreateLimitOrderCommand request, CancellationToken cancellationToken)
     {
