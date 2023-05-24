2a9a7ed7157a4d69f6e5a1fed8ca668c108122d9
MGithubber <mircea.ionescu3000@gmail.com>
2023-05-25 00:52:52 +0300

    Added integration tests for DeleteOrdersCommand

---
+++ Tests/Infrastructure.Tests.Integration/BusinessLogic/Commands/OrdersTests/DeleteOrdersCommandTests.cs
@@ -0,0 +1,72 @@
+﻿using Application.Data.Mapping;
+
+using Bybit.Net.Enums;
+
+using Domain.Commands.Orders;
+using Domain.Commands.Positions;
+
+using FluentAssertions;
+
+using Infrastructure.Tests.Integration.BusinessLogic.Commands.AbstractBase;
+using Infrastructure.Tests.Integration.Common.Fixtures;
+using Infrastructure.Tests.Integration.DataAccess.Extensions;
+
+using Microsoft.EntityFrameworkCore;
+
+using Xunit;
+
+namespace Infrastructure.Tests.Integration.BusinessLogic.Commands.OrdersTests;
+
+public class DeleteOrdersCommandTests : CommandsTestsBase
+{
+    public DeleteOrdersCommandTests(DatabaseFixture databaseFixture) : base(databaseFixture)
+    {
+    }
+
+    
+    [Fact]
+    public async Task DeleteOrdersCommand_ShouldDeleteOrders_WhenSpecifiedIdsExist()
+    {
+        // Arrange
+        var orders = this.FuturesOrdersGenerator.Generate(1000, $"default, {OrderType.Market.ToRuleSetName()}, {OrderSide.Buy.ToRuleSetName()}, {PositionSide.Buy.ToRuleSetName()}");
+        var position = this.FuturesPositionsGenerator.Generate($"default, {PositionSide.Buy.ToRuleSetName()}");
+
+        await this.Mediator.Send(new CreatePositionCommand
+        {
+            Position = position,
+            FuturesOrders = orders,
+        });
+        
+        var command = new DeleteOrdersCommand
+        {
+            BybitIds = orders.Select(x => x.BybitID).ToArray()
+        };
+
+
+        // Act
+        await this.Mediator.Send(command);
+
+        
+        // Assert
+        this.ArrangeAssertDbContext.FuturesOrders.Should().BeEmpty();
+        this.ArrangeAssertDbContext.FuturesPositions.Single().ToDomainObject().Should().BeEquivalentTo(position);
+    }
+
+    [Fact]
+    public async Task DeleteOrdersCommand_ShouldThrow_WhenSpecifiedIdDoesNotExist()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+        var command = new DeleteOrdersCommand
+        {
+            BybitIds = new[] { guid }
+        };
+        
+        // Act
+        var func = async () => await this.Mediator.Send(command);
+        
+        // Assert
+        (await func.Should().ThrowExactlyAsync<DbUpdateException>())
+            .WithMessage($"No order with bybitID {guid} was found in the database");
+    }
+}
