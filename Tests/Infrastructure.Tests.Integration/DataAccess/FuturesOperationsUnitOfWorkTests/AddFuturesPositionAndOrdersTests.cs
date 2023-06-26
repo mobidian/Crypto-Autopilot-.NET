b0d42bf91ab07c864319ce271243e69b93b3bde0
MGithubber <mircea.ionescu3000@gmail.com>
2023-06-26 23:54:10 +0300

    Added an integration test for the IFuturesOperationsService.UpdateFuturesPositionAndAddOrdersAsync method

--- Tests/Infrastructure.Tests.Integration/DataAccess/FuturesOperationsUnitOfWorkTests/AddFuturesPositionAndOrdersTests.cs
+++ Tests/Infrastructure.Tests.Integration/DataAccess/FuturesOperationsUnitOfWorkTests/AddFuturesPositionAndOrdersTests.cs
@@ -1,4 +1,6 @@
-﻿using Application.Data.Mapping;
+﻿using System.Linq;
+
+using Application.Data.Mapping;
 
 using Bybit.Net.Enums;
 
@@ -20,7 +22,7 @@ public class AddFuturesPositionAndOrdersTests : FuturesOperationsServiceTestsBas
 
 
     [Fact]
-    public async Task AddFuturesPosition_ShouldAddFuturesPositionAndOrders_WhenAllFuturesOrdersRequirePosition()
+    public async Task AddFuturesPositionAndOrders_ShouldAddFuturesPositionAndOrders_WhenAllFuturesOrdersRequirePosition()
     {
         // Arrange
         var position = this.FuturesPositionsGenerator.Generate($"default, {PositionSide.Buy.ToRuleSetName()}");
@@ -32,5 +34,27 @@ public class AddFuturesPositionAndOrdersTests : FuturesOperationsServiceTestsBas
         // Assert
         this.ArrangeAssertDbContext.FuturesPositions.Single().ToDomainObject().Should().BeEquivalentTo(position);
         this.ArrangeAssertDbContext.FuturesOrders.Select(x => x.ToDomainObject()).Should().BeEquivalentTo(orders);
+    }
+
+    [Fact]
+    public async Task UpdateFuturesPositionAndAddOrders_ShouldUpdateFuturesPositionAndAddOrders_WhenPositionExistsAndAllFuturesOrdersRequirePosition()
+    {
+        // Arrange
+        var position = this.FuturesPositionsGenerator.Generate($"default, {PositionSide.Buy.ToRuleSetName()}");
+        var orders = this.FuturesOrdersGenerator.Generate(10, $"default, {OrderType.Market.ToRuleSetName()}, {OrderSide.Buy.ToRuleSetName()}, {PositionSide.Buy.ToRuleSetName()}");
+
+        var updatedPosition = this.FuturesPositionsGenerator.Clone().RuleFor(x => x.CryptoAutopilotId, position.CryptoAutopilotId).Generate($"default, {PositionSide.Buy.ToRuleSetName()}");
+        var newOrders = this.FuturesOrdersGenerator.Generate(3, $"default, {OrderType.Market.ToRuleSetName()}, {OrderSide.Sell.ToRuleSetName()}, {PositionSide.Buy.ToRuleSetName()}");
+
+        await this.SUT.AddFuturesPositionAndOrdersAsync(position, orders);
+
+
+        // Act
+        await this.SUT.UpdateFuturesPositionAndAddOrdersAsync(updatedPosition, newOrders);
+
+
+        // Assert
+        this.ArrangeAssertDbContext.FuturesPositions.Single().ToDomainObject().Should().BeEquivalentTo(updatedPosition);
+        this.ArrangeAssertDbContext.FuturesOrders.Select(x => x.ToDomainObject()).Should().BeEquivalentTo(orders.Concat(newOrders));
     }
 }
