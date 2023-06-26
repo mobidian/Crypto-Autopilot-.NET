40c535a10e4f0a813279cf1f3150b44818d6167b
MGithubber <mircea.ionescu3000@gmail.com>
2023-06-26 23:03:45 +0300

    Added command to update multiple positions and a validator for it

---
+++ Source/Domain/Commands/Positions/UpdatePositionsCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Domain.Commands.Positions;
+
+/// <summary>
+/// Represents a command that is sent when multiple existing positions are updated.
+/// </summary>
+public class UpdatePositionsCommand : IRequest<Unit>
+{
+    /// <summary>
+    /// Gets or initializes the commands that will be executed to update the positions.
+    /// </summary>
+    public IEnumerable<UpdatePositionCommand> Commands { get; init; } = Enumerable.Empty<UpdatePositionCommand>();
+}
---
+++ Source/Domain/Validation/Commands/Positions/UpdatePositionsCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Commands.Positions;
+
+using FluentValidation;
+
+namespace Domain.Validation.Commands.Positions;
+
+public class UpdatePositionsCommandValidator : AbstractValidator<UpdatePositionsCommand>
+{
+    private static readonly UpdatePositionCommandValidator UpdatePositionCommandValidator = new();
+
+    public UpdatePositionsCommandValidator()
+    {
+        this.RuleFor(command => command.Commands).NotNull();
+
+        this.RuleForEach(command => command.Commands).NotNull();
+        this.RuleForEach(command => command.Commands).SetValidator(UpdatePositionCommandValidator);
+    }
+}
