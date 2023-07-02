8b9a960b271d3c9309cc071befda3b183eed2687
MGithubber <mircea.ionescu3000@gmail.com>
2023-07-03 00:11:53 +0300

    Added a migration for the trading signals database table

---
+++ Source/Infrastructure/DataAccess/Database/Migrations/20230702210846_AddTradingSignalsTableMigration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTradingSignalsTableMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TradingSignals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CryptoAutopilotId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Source = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CurrencyPair = table.Column<string>(name: "Currency Pair", type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Info = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TradingSignals", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TradingSignals_CryptoAutopilotId",
+                table: "TradingSignals",
+                column: "CryptoAutopilotId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TradingSignals");
+        }
+    }
+}
