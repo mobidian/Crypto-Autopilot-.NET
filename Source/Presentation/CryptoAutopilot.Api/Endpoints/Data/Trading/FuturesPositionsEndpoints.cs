dee6010180f8365b23187ffc5faeb139b9c27542
MGithubber <mircea.ionescu3000@gmail.com>
2023-07-21 23:52:14 +0300

    Moved the "Data/Trading/Positions" into api

---
+++ Source/Presentation/CryptoAutopilot.Api/Endpoints/Data/Trading/FuturesPositionsEndpoints.cs
@@ -0,0 +1,65 @@
+﻿using Application.DataAccess.Repositories;
+
+using CryptoAutopilot.Api.Endpoints.Internal.Automation.General;
+using CryptoAutopilot.Contracts.Responses.Common;
+using CryptoAutopilot.Contracts.Responses.Data.Trading.Positions;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoAutopilot.Api.Endpoints.Data.Trading;
+
+public class FuturesPositionsEndpoints : IEndpoints
+{
+    public static void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+    }
+
+    public static void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet("Data/Trading/Positions", async ([FromQuery] string? contractName, IFuturesPositionsRepository positionsRepository) =>
+        {
+            if (contractName is null)
+            {
+                var futuresPositions = await positionsRepository.GetAllAsync();
+
+                var futuresPositionsResponses = futuresPositions.Select(x => new FuturesPositionResponse
+                {
+                    CryptoAutopilotId = x.CryptoAutopilotId,
+                    ContractName = x.CurrencyPair.Name,
+                    Side = x.Side,
+                    Margin = x.Margin,
+                    Leverage = x.Leverage,
+                    Quantity = x.Quantity,
+                    EntryPrice = x.EntryPrice,
+                    ExitPrice = x.ExitPrice,
+                });
+                var response = new GetAllFuturesPositionsResponse { FuturesPositions = futuresPositionsResponses };
+
+                return Results.Ok(response);
+            }
+            else
+            {
+                var futuresPositions = await positionsRepository.GetByCurrencyPairAsync(contractName);
+
+                var futuresPositionsResponses = futuresPositions.Select(x => new FuturesPositionResponse
+                {
+                    CryptoAutopilotId = x.CryptoAutopilotId,
+                    ContractName = x.CurrencyPair.Name,
+                    Side = x.Side,
+                    Margin = x.Margin,
+                    Leverage = x.Leverage,
+                    Quantity = x.Quantity,
+                    EntryPrice = x.EntryPrice,
+                    ExitPrice = x.ExitPrice
+                });
+                var response = new GetFuturesPositionsByContractNameResponse
+                {
+                    ContractName = contractName.ToUpper(),
+                    FuturesPositions = futuresPositionsResponses,
+                };
+
+                return Results.Ok(response);
+            }
+        });
+    }
+}
