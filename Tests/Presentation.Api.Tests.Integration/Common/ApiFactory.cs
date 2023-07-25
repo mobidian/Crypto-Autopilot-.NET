1976f813cad988dd125cd477af7d4dbcf325bdc2
MGithubber <mircea.ionescu3000@gmail.com>
2023-07-25 23:09:50 +0300

    Added a DatabaseFixture dependency on the ApiFactory

--- Tests/Presentation.Api.Tests.Integration/Common/ApiFactory.cs
+++ Tests/Presentation.Api.Tests.Integration/Common/ApiFactory.cs
@@ -4,12 +4,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Logging;
 
+using Tests.Integration.Common.Fixtures;
+
+using Xunit;
+
 namespace Presentation.Api.Tests.Integration.Common;
 
+[Collection(nameof(DatabaseFixture))]
 public class ApiFactory : WebApplicationFactory<IApiMarker>
 {
+    private readonly string ConnectionString;
+
+    public ApiFactory(DatabaseFixture databaseFixture)
+    {
+        this.ConnectionString = databaseFixture.ConnectionString;
+    }
+
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureLogging(logging => logging.ClearProviders());
+
+        builder.ConfigureAppConfiguration((ctx, _) => ctx.Configuration["ConnectionStrings:TradingHistoryDB"] = this.ConnectionString);
     }
 }
